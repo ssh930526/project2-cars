0874cf20bd7446a8a5f0bcd44025d1b62729774f
Sihui Shen <sihui@Sihuis-MacBook-Pro.local>
2021-04-24 18:32:37 -0500

    wipcommiit all new changes

--- public/js/script.js
+++ public/js/script.js
@@ -1,22 +1,16 @@
 //constant variables - data that naver changes
-const BASE_URL = 'https://raw.githubusercontent.com/vega/vega/master/docs/data/cars.json';
-
+const BASE_URL = 'https://raw.githubusercontent.com/vega/vega/master/docs/data/cars.json?limit=18';
 //state variable - data that changes
 let cars;
-
 //cached element references - parts of the dom we need to touch
 const $cars = $('#cars');
-
 //event listeners -capture and respond to event i.e. user clicks on somethings
-
 $cars.on('click', '.sort', handleShowModal);
 //function - code that represents actions taken/carried out
-
 init();
 function init() {
     getData();
 }
-
 function handleShowModal(){
     const carName= parseInt(this.dataset.carName);
     console.log(this.dataset);
@@ -24,37 +18,31 @@ function handleShowModal(){
         return car.name === carName;
     });
     console.log(selectedCar);
-
-
 // add the content to the modal
-
 $('#name').text(selectedCar.name);
-
 $('#year').text(selectedCar.year);
-
 $('#origin').text(selectedCar.origin);
-
 $('.modal').modal();
 }
-
 function getData() {
-    $.ajax(BASE_URL + "?limit=18")
-    .then(function render(cars){
-        // console.log(data);
-        cars = data;
-        render();
+    $.ajax(BASE_URL)
+    .then(function (data){
+        console.log(typeof(data));
+        cars = JSON.parse(data).slice(0,18);
+        render(cars);
     }, function (error) {
         console.log(error);
     });
 }
-
 function render(cars) {
     const html = cars.map(function(car){
-    
         return `
-        <article data-car-name="${car.name}" class="sort">
-                <h1>${car.name}</h1>
-                <p>${car.year}</p>
+        <article data-car-name="${car.Name}" class="sort">
+                <h1>Car Name: ${car.Name}</h1>
+                <p>Year: ${car.Year}</p>
+                <p>Origin: ${car.Origin}</p>
+                <p>Cylinder: ${car.Cylinders}</p>
+                <p>Miles/Gallon: ${car.Miles_per_Gallon}</p>
             </article>
         `;
     });
